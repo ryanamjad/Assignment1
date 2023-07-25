8f1cac0dd9aaae9e03575d5a7ce4d094931e315d
Ryan Amjad <73199107+ryanamjad@users.noreply.github.com>
2023-07-25 19:30:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const navbarToggle = document.getElementById('navbarToggle');
+const navbarLinks = document.getElementById('navbarLinks');
+
+navbarToggle.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
